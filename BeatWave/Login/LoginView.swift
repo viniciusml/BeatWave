93a9455dbdddc18a2bf9f3f30931ca9a103a75b0
Vinicius Leal <viniciusmoreira.leal@thetrainline.com>
2024-09-02 21:19:24 +0100

    Add basic LoginView and extension for HEX colors

---
+++ BeatWave/Login/LoginView.swift
@@ -0,0 +1,58 @@
+//
+//  LoginView.swift
+//  BeatWave
+//
+//  Created by Vinicius Leal on 02/09/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+                
+                VStack(spacing: 10) {
+                    Image(systemName: "bolt.fill")
+                        .font(.system(size: 50))
+                        .foregroundColor(.white)
+                    Text("BeatWave")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                }
+                
+                Spacer()
+                
+                // Add text views here.
+                
+                Button(action: {
+                    // Login Action
+                }) {
+                    Text("Log in")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal, 40)
+                
+                Text("© 2024 BeatWave")
+                    .font(.footnote)
+                    .foregroundColor(.white)
+                    .padding(.top, 40)
+                
+                Spacer().frame(height: 20)
+            }
+            .background(Color.red)
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
