7e3965a9193cb3598ae7b8e9ff5b404d52dd74fa
Vinicius Leal <viniciusmoreira.leal@thetrainline.com>
2024-09-02 21:04:46 +0100

    Initial Commit

---
+++ BeatWave/BeatWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatWaveApp.swift
+//  BeatWave
+//
+//  Created by Vinicius Leal on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BeatWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
